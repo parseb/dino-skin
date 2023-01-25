febc1f2bde044a06013ca173f3326356707dd367
parseb <petra306@protonmail.com>
2023-01-25 22:42:23 +0200

    basic dino

--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -1,31 +1,50 @@
 <script>
-    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
   
   </script>
 
+<div class="container">
+<br>
+<br>
 <br>
 
-<h5>
-    dino <br>
-    skin    
-    <hr>
-</h5>
+<div class="row">
+    <div class="col-6">
+        <div class="dino-title">
+            <h5 class="text-muted"> <b>d</b> ecentralized</h5>
+            <h5> <b>i</b> nternet</h5>
+            <h5> <b>n</b> ative</h5>
+            <h5 class="text-muted"> <b>o</b> rganization</h5>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="dinosaur">
+            <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_EUyLAK.json"  
+            background="transparent"  
+            speed="0.8"  
+            style="width: auto; height: auto;" autoplay ></lottie-player>
+        </div>
+    </div>
+</div>
 
-<LottiePlayer
-  src="https://assets3.lottiefiles.com/packages/lf20_EUyLAK.json"
-  autoplay="{false}"
-  loop="{true}"
-  hover="{true}"
-  controls="{false}"
-  renderer="svg"
-  background="transparent"
-  height="{400}"
-  width="{400}"
-/>
+</div>
 
 <style>
     h5 {
         font-family: 'Courier New', Courier, monospace;
         font-size: large;
+    }
+
+    .dinosaur {
+        padding: 80px
+    }
+
+    :global(body) {
+        background-color: #f2eee2;
+        color: #0084f6;
+        transition: background-color 0.3s
+    }
+    :global(body.dark-mode) {
+        background-color: #1d3040;
+        color: #bfc2c7;
     }
 </style>
